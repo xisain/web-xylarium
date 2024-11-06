dd59c8c6a80a4c9523a513cc5902ed9bc4c7e646
xisain <husenabs232bgr@gmail.com>
2024-11-07 06:41:39 +0700

    "Comment out forcing HTTPS scheme in local environment in AppServiceProvider"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-            if (config('app.env') === 'local') {
-                URL::forceScheme('https');
-            }
+            // if (config('app.env') === 'local') {
+            //     URL::forceScheme('https');
+            // }
     }
 
     /**
